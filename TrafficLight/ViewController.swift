069956656a230d2b1668ed7b2464e3306f118b95
kirillslyusar <kirillslyusar@gmail.com>
2023-04-24 23:55:24 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Кирилл Слюсарь on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
