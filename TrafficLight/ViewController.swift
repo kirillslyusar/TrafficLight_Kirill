17fcba861ca98c81a26b44e6f94eccdb9899217c
kirillslyusar <kirillslyusar@gmail.com>
2023-06-05 17:55:59 +0300

    3

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,14 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yollowLight: UIView!
+    @IBOutlet var redLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+
+    private var currentLight = Currentlight.yellow
+    private let lifhtIsOn : CGFloat = 1
+    private let lightIsOff :  CGFloat = 0.3
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        startButton.layer.cornerRadius = 10
+        
+        greenLight.alpha = lightIsOff
+        yollowLight.alpha = lightIsOff
+        redLight.alpha = lightIsOff
+        
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        yollowLight.layer.cornerRadius = yollowLight.frame.width / 2
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "Start" {
+        
+        }
+        
+        switch currentLight {
+        case .green:
+            yollowLight.alpha = lightIsOff
+            redLight.alpha = lightIsOff
+            currentLight = .green
+            
+        case .yellow:
+            yollowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOff
+            currentLight = .yellow
+            
+        case .red:
+            greenLight.alpha = lightIsOff
+            yollowLight.alpha = lightIsOff
+            currentLight = .red
+            
+            
+        }
+    }
+    
+    
 }
+//MARK: - Currentlight
+    
+    extension ViewController {
+        private enum Currentlight {
+            case green, yellow, red
+        }
+    }
+    
 
